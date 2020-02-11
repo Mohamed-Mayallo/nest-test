5c725577e1eaed7a49248d1963ee32ffbb07408d
baianat <solutions@baianat.com>
2020-02-11 11:10:31 +0200

    Refine database configuration

--- src/databases/databases.config.ts
+++ src/databases/databases.config.ts
@@ -2,39 +2,27 @@ import { SequelizeOptions } from 'sequelize-typescript';
 import { ConfigService } from '@nestjs/config';
 
 export const config = (configService: ConfigService) => {
-  const stages = {
-    development: <SequelizeOptions>{
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: configService.get('DB_USER'),
-      password: configService.get('DB_PASS'),
-      database: configService.get('DB_NAME'),
-      models: [process.cwd() + '/**/*.entity.{ts,js}'], // At dev env, dist will be generated so will read from dist => *.js
-      modelMatch: (filename, member) => {
-        return (
-          filename.substring(0, filename.indexOf('.entity')) ===
-          member.toLowerCase()
-        );
-      }
-    },
-    production: <SequelizeOptions>{},
-    test: <SequelizeOptions>{
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: configService.get('DB_USER'),
-      password: configService.get('DB_PASS'),
-      database: configService.get('TEST_DB_NAME'),
-      models: [process.cwd() + '/**/*.entity.{ts,js}'], // At test env, no dist will be generated so will read from *.ts
-      modelMatch: (filename, member) => {
-        return (
-          filename.substring(0, filename.indexOf('.entity')) ===
-          member.toLowerCase()
-        );
-      }
+  return <SequelizeOptions>{
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: configService.get('DB_USER'),
+    password: configService.get('DB_PASS'),
+    database:
+      configService.get('NODE_ENV') === 'test'
+        ? configService.get('TEST_DB_NAME')
+        : configService.get('DB_NAME'),
+    models:
+      configService.get('NODE_ENV') === 'test'
+        ? // At test env, no dist will be generated so will read from *.ts
+          [process.cwd() + '/**/*.entity.ts']
+        : // At dev env, dist will be generated so will read from dist => *.js
+          [process.cwd() + '/**/*.entity.js'],
+    modelMatch: (filename, member) => {
+      return (
+        filename.substring(0, filename.indexOf('.entity')) ===
+        member.toLowerCase()
+      );
     }
   };
-
-  return stages[configService.get('NODE_ENV')];
 };
