540adc8f7204b08a51f9318bb917f06838b777e3
baianat <solutions@baianat.com>
2020-02-12 12:12:06 +0200

    Setup Nest scheduling auto db backup process

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import * as path from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UsersModule } from './users/users.module';
 import { DatabasesModule } from './databases/databases.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { UploadModule } from './upload/upload.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { GqlConfigService } from './graphql.provider';
@@ -11,9 +14,16 @@ import { AuthService } from './auth/auth.service';
 import { LoggerModule } from './logger/logger.module';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './exceptions/exception-filter';
+import { ScheduleModule } from '@nestjs/schedule';
+import { AutoDbBackupService } from './shared/auto-db-backup.service';
+
+const env = dotenv.parse(fs.readFileSync(path.join(__dirname, '../.env')));
 
 @Module({
-  providers: [{ provide: APP_FILTER, useClass: HttpExceptionFilter }],
+  providers: [
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+    ...(env.NODE_ENV === 'production' ? [AutoDbBackupService] : [])
+  ],
   exports: [],
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
@@ -27,7 +37,8 @@ import { HttpExceptionFilter } from './exceptions/exception-filter';
     UploadModule,
     UsersModule,
     AuthModule,
-    LoggerModule.forRoot()
+    LoggerModule.forRoot(),
+    ScheduleModule.forRoot()
   ]
 })
 export class AppModule {}
